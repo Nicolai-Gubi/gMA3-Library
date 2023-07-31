c6f63ced080b0f18a2168376f6e756009012f854
Nicolai-Gubi <113240111+Nicolai-Gubi@users.noreply.github.com>
2023-07-31 16:35:25 +0200

    Create enums.lua

---
+++ definitions/enums.lua
@@ -0,0 +1,635 @@
+---@meta
+
+--
+-- These are all the enums in the grandMA3 system. 
+-- Most are not described yet in this definition.
+--
+-- This list is from version 1.9.3.3
+-- This list is currently 465 enums
+--
+--
+
+---@enum Enums.ALSideSizeSpecial
+---@enum Enums.ActionMode
+---@enum Enums.ActiveDisplay
+---@enum Enums.AgendaMode
+---@enum Enums.AgendaTool
+---@enum Enums.AgendaViewMode
+---@enum Enums.AlignMath
+---@enum Enums.AlignMode
+---@enum Enums.AlignmentH
+---@enum Enums.AlignmentV
+---@enum Enums.ArrangementMarcType
+---@enum Enums.ArtNetBroadcastThreshold
+---@enum Enums.ArtNetDataMode
+---@enum Enums.AssignType
+---@enum Enums.AssignmentBlindButtonFunctions
+---@enum Enums.AssignmentButtonFunctions
+---@enum Enums.AssignmentButtonFunctionsGroup
+---@enum Enums.AssignmentButtonFunctionsMacro
+---@enum Enums.AssignmentButtonFunctionsPlugin
+---@enum Enums.AssignmentButtonFunctionsPreset
+---@enum Enums.AssignmentButtonFunctionsScreenConfig
+---@enum Enums.AssignmentButtonFunctionsSequence
+---@enum Enums.AssignmentButtonFunctionsSoundFile
+---@enum Enums.AssignmentButtonFunctionsUser
+---@enum Enums.AssignmentButtonFunctionsView
+---@enum Enums.AssignmentButtonFunctionsWorld
+---@enum Enums.AssignmentFaderFunctions
+---@enum Enums.AssignmentFaderFunctionsMasterOnly
+---@enum Enums.AssignmentFaderFunctionsNone
+---@enum Enums.AssignmentGrandMasterButtonFunctions
+---@enum Enums.AssignmentHighlightSoloButtonFunctions
+---@enum Enums.AssignmentPlaybackMasterButtonFunctions
+---@enum Enums.AssignmentProgExecButtonFunctions
+---@enum Enums.AssignmentQuickeyFunctions
+---@enum Enums.AssignmentRateButtonFunctions
+---@enum Enums.AssignmentSoundMasterButtonFunctions
+---@enum Enums.AssignmentSpeedButtonFunctions
+---@enum Enums.AssignmentTimecodeFunctions
+---@enum Enums.AssignmentTimerFunctions
+---@enum Enums.AttributeMode
+---@enum Enums.AttriebuteEncoderResolution
+---@enum Enums.AttriebuteEncoderResolutionDefault
+---@enum Enums.AttriebuteEncoderResolutionSmall
+---@enum Enums.AutoInterface
+---@enum Enums.AutoLayoutScrollType
+---@enum Enums.AutoSaveMode
+---@enum Enums.AutoStomp
+---@enum Enums.AxisGroupType
+---@enum Enums.AxisSystem
+---@enum Enums.BackdropPatchType
+---@enum Enums.BackupBrowserFilter
+---@enum Enums.BeamMode
+---@enum Enums.BeamModePatch
+---@enum Enums.BeamType
+---@enum Enums.BlinkingButtonMode
+---@enum Enums.BloomIntensity3d
+---@enum Enums.BodyQuality3d
+---@enum Enums.BodyQuality3dPatch
+---@enum Enums.BuildType
+---@enum Enums.ButtonHeight
+---@enum Enums.CachedObjectSource
+---@enum Enums.CalculatorMode
+---@enum Enums.CameraMode
+---@enum Enums.CameraType
+---@enum Enums.CellGrouping
+---@enum Enums.ChangeLevel
+---@enum Enums.ChannelFilterMode
+---@enum Enums.ChannelFrequency
+---@enum Enums.ChildrenFillPolicy
+---@enum Enums.ClockSources
+---@enum Enums.CmdEventStatus
+---@enum Enums.ColorDisplayMode
+---@enum Enums.ColorDisplayModeAuto
+---@enum Enums.ColorEncoderFunction
+---@enum Enums.ColorMixMode
+---@enum Enums.ColorPickerWindowMode
+---@enum Enums.ColorSample
+---@enum Enums.ColorSpaceMode
+---@enum Enums.ColorWheelMode
+---@enum Enums.ColumnOrder
+---@enum Enums.CommandWingBarDisplayMode
+---@enum Enums.Config
+---@enum Enums.ConnectionLimitConsole
+---@enum Enums.ConnectionLimitOnPC
+---@enum Enums.ConnectorGender
+---@enum Enums.ConnectorType
+---@enum Enums.ContentSheetCueMode
+---@enum Enums.CookMode
+---@enum Enums.CopyCueOnly
+---@enum Enums.CountdownAlertRange
+---@enum Enums.CountdownAlertType
+---@enum Enums.CreateBy
+---@enum Enums.CueCopyDst
+---@enum Enums.CueCopyDstCmd
+---@enum Enums.CueCopyDstMode
+---@enum Enums.CueCopySrc
+---@enum Enums.CueOnly
+---@enum Enums.CuePartAppearance
+---@enum Enums.CuePartMode
+---@enum Enums.CuePartTextures
+---@enum Enums.CueTrigger
+---@enum Enums.DMXBreak
+---@enum Enums.DMXMergeMode
+---@enum Enums.DMXReadoutMode
+---@enum Enums.DMXValueReadoutMode
+---@enum Enums.DMXVirtualResolution
+---@enum Enums.DSCPLevel
+---@enum Enums.DatumMode
+---@enum Enums.DebugUsbDataSection
+---@enum Enums.DeskLightChannel
+---@enum Enums.DimmerWheelResolution
+---@enum Enums.DispView
+---@enum Enums.DisplayIndex
+---@enum Enums.DisplayNone
+---@enum Enums.DisplayNumber
+---@enum Enums.DisplayScales
+---@enum Enums.DisplayType
+---@enum Enums.DisplayTypePreference
+---@enum Enums.DmxCurveEditTool
+---@enum Enums.DmxCurveModes
+---@enum Enums.DmxPrio
+---@enum Enums.DmxSheetCellType
+---@enum Enums.DmxSheetFixedColumnType
+---@enum Enums.DmxSheetHeaderType
+---@enum Enums.DmxSheetSettingsAddressMode
+---@enum Enums.DmxSheetSettingsLevelbar
+---@enum Enums.DmxSheetSettingsSelected
+---@enum Enums.DmxSheetSettingsShowField
+---@enum Enums.DmxSheetSettingsViewMode
+---@enum Enums.DmxState
+---@enum Enums.DmxTesterAddressMode
+---@enum Enums.DmxTesterMode
+---@enum Enums.DriveType
+---@enum Enums.DynamicPresetPool
+---@enum Enums.ECPlay
+---@enum Enums.ECRec
+---@enum Enums.EmptyAsZero
+---@enum Enums.EmptyAsZeroFID
+---@enum Enums.EncoderFactor
+---@enum Enums.EncoderFunction
+---@enum Enums.EncoderFunctionLayoutView
+---@enum Enums.EncoderLink
+---@enum Enums.EncoderLinkPhaser
+---@enum Enums.EncoderLinkResolution
+---@enum Enums.EncoderLinkValues
+---@enum Enums.EncoderRing
+---@enum Enums.EncoderType
+---@enum Enums.ExecConfigType
+---@enum Enums.ExecDisplayMode
+---@enum Enums.ExecEditorTab
+---@enum Enums.ExitCode
+---@enum Enums.ExtraStatusInfo
+---@enum Enums.Fade
+---@enum Enums.FaderEnable
+---@enum Enums.FaderFunctions
+---@enum Enums.FalloffType
+---@enum Enums.FixedColumns
+---@enum Enums.FixturGraphicSource
+---@enum Enums.FixtureAppearanceMode
+---@enum Enums.FixtureGraphicMode
+---@enum Enums.FixtureIdEnum
+---@enum Enums.FixtureInvert
+---@enum Enums.FixtureSheetFixedColumnType
+---@enum Enums.FixtureSheetHeaderType
+---@enum Enums.FixtureTypeSource
+---@enum Enums.FixtureTypeSourceImport
+---@enum Enums.FixtureTypeXYZStatus
+---@enum Enums.FocusPriority
+---@enum Enums.FocusReason
+---@enum Enums.FocusSearchPolicy
+---@enum Enums.FontSizeType
+---@enum Enums.FontSizes
+---@enum Enums.FrameFormat
+---@enum Enums.FrameFormatClockSource
+---@enum Enums.FrameFormatDefault
+---@enum Enums.FunctionMode
+---@enum Enums.GelGridType
+---@enum Enums.GelSortType
+---@enum Enums.GeneratorLimits
+---@enum Enums.GeometryType
+---@enum Enums.GestureId
+---@enum Enums.GestureResult
+---@enum Enums.GestureStatus
+---@enum Enums.GoboMode
+---@enum Enums.GrandKnob
+---@enum Enums.GridColumnFilterCollect
+---@enum Enums.GridContentFilterMode
+---@enum Enums.GridMergeMode
+---@enum Enums.GridModeAgenda
+---@enum Enums.GridSortOrder
+---@enum Enums.GridTool
+---@enum Enums.GroupMasterMode
+---@enum Enums.GroupMemoryType
+---@enum Enums.GroupSelectionType
+---@enum Enums.HostOs
+---@enum Enums.HostStatus
+---@enum Enums.HostSubType
+---@enum Enums.HostType
+---@enum Enums.IgnoreFT
+---@enum Enums.ImageBackGroundMode
+---@enum Enums.ImageMirror
+---@enum Enums.ImageRotation
+---@enum Enums.ImageSource
+---@enum Enums.ImportExportObjectType
+---@enum Enums.Index
+---@enum Enums.InfoAutoListRef
+---@enum Enums.InfoLinkMode
+---@enum Enums.InputControl3d
+---@enum Enums.InputType
+---@enum Enums.InvalidFootprint
+---@enum Enums.KeyAction
+---@enum Enums.KeyFunctions
+---@enum Enums.KeyboardCodes
+---@enum Enums.KeyboardModifier
+---@enum Enums.LampType
+---@enum Enums.LayoutElementAlignmentV
+---@enum Enums.LayoutElementSelectionRelevance
+---@enum Enums.LayoutFitType
+---@enum Enums.LayoutGridStyle
+---@enum Enums.LayoutOutputSelection
+---@enum Enums.LayoutSizePolicy
+---@enum Enums.LayoutStretch
+---@enum Enums.LayoutTool
+---@enum Enums.LayoutType
+---@enum Enums.LayoutVisibility
+---@enum Enums.LicenseRequest
+---@enum Enums.LineHeights
+---@enum Enums.LoopMode
+---@enum Enums.MIDIMode
+---@enum Enums.MacroLineWait
+---@enum Enums.Master
+---@enum Enums.MasterPriority
+---@enum Enums.MasterReaction
+---@enum Enums.MatricksBlockNone
+---@enum Enums.MatricksGroupNone
+---@enum Enums.MatricksIndexNone
+---@enum Enums.MatricksInvert
+---@enum Enums.MatricksInvertStyle
+---@enum Enums.MatricksShiftNone
+---@enum Enums.MatricksShuffleNone
+---@enum Enums.MatricksTabs
+---@enum Enums.MatricksWingsNone
+---@enum Enums.MatrixWidthAuto
+---@enum Enums.MeasurementInterpolation
+---@enum Enums.MeasurementStatus
+---@enum Enums.MessageCategory
+---@enum Enums.MessagePriority
+---@enum Enums.MibEnableMode
+---@enum Enums.MibMode
+---@enum Enums.MibModeSequence
+---@enum Enums.MibMultiStep
+---@enum Enums.MibPreferenceLevel
+---@enum Enums.MibTiming
+---@enum Enums.ModalResult
+---@enum Enums.Month
+---@enum Enums.MonthShort
+
+---@enum Enums.MouseButtonTypes
+local MouseButtonTypes = {
+    Left = 0,
+    Right = 1,
+    Middle = 2
+}
+
+---@enum Enums.MultiLedBeamMode
+---@enum Enums.NetworkFilter
+---@enum Enums.NoName
+---@enum Enums.None
+---@enum Enums.OSCMode
+---@enum Enums.OffCueTrigger
+---@enum Enums.OnOff
+---@enum Enums.OnOffStatus
+---@enum Enums.OopsConfirmation
+---@enum Enums.OutputDelay
+---@enum Enums.PatchOffset
+---@enum Enums.PatchType
+---@enum Enums.Patched
+---@enum Enums.PathContentType
+
+---@enum Enums.PathType
+local PathType = {
+    BinaryDir = 0,
+    ExternalRoot = 1,
+    ExternalPackages = 2,
+    MADir = 3,
+    VersionDir = 4,
+    InstallationPackages = 5,
+    UpdateDir = 6,
+    WebDaemon = 7,
+    Data = 8,
+    Shared = 9,
+    Config = 10,
+    Usb = 11,
+    Temp = 12,
+    Undo = 13,
+    CrashLog = 14,
+    UploadedCrashLogs = 15,
+    InvalidCrashLogs = 16,
+    Showfiles = 17,
+    Backupfiles = 18,
+    DemoShowfiles = 19,
+    Resource = 20,
+    WebResource = 21,
+    Language = 22,
+    ColorTheme = 23,
+    Shaders = 24,
+    Textures = 25,
+    Fonts = 26,
+    GoboImageLibrary = 27,
+    GoboImageCache = 28,
+    Mesh = 29,
+    MaterialLibrary = 30,
+    RenderQuality = 31,
+    SymbolImageLibrary = 32,
+    GelLibrary = 33,
+    AgendaLibrary = 34,
+    AddonLibrary = 35,
+    MenuLibrary = 36,
+    PluginLibrary = 37,
+    MacroLibrary = 38,
+    QuickeyLibrary = 39,
+    MatricksLibrary = 40,
+    PresetLibrary = 41,
+    FixtureLibrary = 42,
+    GrandMA2Library = 43,
+    GrandMA3Library = 44,
+    Keyboards = 45,
+    KeyboardShortcuts = 46,
+    Software = 47,
+    VizLibrary = 48,
+    MvrLibrary = 49,
+    DmxCurvesLibrary = 50,
+    Library = 51,
+    TemplateShowfiles = 52,
+    UserMedia = 53,
+    SoundLibrary = 54,
+    VideoLibrary = 55,
+    ImageLibrary = 56,
+    UserSymbols = 57,
+    ScribbleLibrary = 58,
+    AppearanceLibrary = 59,
+    NetworkKeys = 60,
+    UserInOut = 61,
+    OSC = 62,
+    SACN = 63,
+    ArtNet = 64,
+    UserRemotesDC = 65,
+    UserRemotesDMX = 66,
+    UserRemotesMIDI = 67,
+    UserOutputConfig = 68,
+    UserDeviceConfig = 69,
+    UserFixtures = 70,
+    UserFixtureTypeResources = 71,
+    UserGobos = 72,
+    UserMeshes = 73,
+    UserMeshImages = 74,
+    UserMaterials = 75,
+    UserMvr = 76,
+    UserUsers = 77,
+    UserProfiles = 78,
+    UserViews = 79,
+    ViewButtonLibrary = 80,
+    UserCameras = 81,
+    UserScreenConfigurations = 82,
+    UserRenderQuality = 83,
+    UserKeyboardShortcuts = 84,
+    UserAgendas = 85,
+    UserCertificates = 86,
+    UserColorThemes = 87,
+    UserDataPools = 88,
+    CustomPluginLibrary = 89,
+    UserLayouts = 90,
+    UserExecConfigs = 91,
+    UserMacros = 92,
+    UserMatricks = 93,
+    UserPresets = 94,
+    UserGroups = 95,
+    UserSequences = 96,
+    UserWorlds = 97,
+    UserFilters = 98,
+    UserExecutors = 99,
+    UserTimecodes = 100,
+    UserTimers = 101,
+    UserPatch = 102,
+    UserStages = 103,
+    UserDmxCurves = 104
+}
+
+---@enum Enums.PhaseValueNone
+---@enum Enums.PhaserAbsRelMode
+---@enum Enums.PhaserBars
+---@enum Enums.PhaserEditTool
+---@enum Enums.PhaserEncoderFunction
+---@enum Enums.PhaserFields
+---@enum Enums.PhaserLineHeight
+---@enum Enums.PhaserMode1D
+---@enum Enums.PhaserSheetMode
+---@enum Enums.PhaserTransformations
+---@enum Enums.PhaserTransformationsShort
+---@enum Enums.PhaserValueFields
+---@enum Enums.PhaserViewMode
+---@enum Enums.PhysicalUnit
+---@enum Enums.PlaybackMaster
+---@enum Enums.PlaybackPriority
+---@enum Enums.PlaybackSourceSubTypes
+---@enum Enums.PlaybackWindowExecFrom
+---@enum Enums.PlaybacksOff
+---@enum Enums.PlaybacksToShow
+---@enum Enums.PluginPlacement
+---@enum Enums.PoolSizeFactor
+---@enum Enums.PresetAction
+---@enum Enums.PresetDisplayMode
+---@enum Enums.PresetFilterMode
+---@enum Enums.PresetIsPlayback
+---@enum Enums.PresetLinkMode
+---@enum Enums.PresetMode
+---@enum Enums.PresetModeDefault
+---@enum Enums.PresetReadoutMode
+---@enum Enums.PresetValuesMode
+---@enum Enums.Pretty RDMSensorUnit
+---@enum Enums.PrettyRDMSensorUnitPrefix
+---@enum Enums.ProgLayer
+---@enum Enums.ProgLayerAuto
+---@enum Enums.ProgUpdateCueMode
+---@enum Enums.ProgValueSource
+---@enum Enums.ProgrammingLayer
+---@enum Enums.ProgrammingLayerGroup
+---@enum Enums.PropertyRadioButtonListEnabledItems
+---@enum Enums.PropertyRadioButtonListSetType
+---@enum Enums.PultType
+---@enum Enums.RDMCommandClass
+---@enum Enums.RDMDataType
+---@enum Enums.RDMLampOnMode
+---@enum Enums.RDMLampState
+---@enum Enums.RDMManufacturerId
+---@enum Enums.RDMNotificationThresholdOperator
+---@enum Enums.RDMPID
+---@enum Enums.RDMParameterCommand
+---@enum Enums.RDMParameterType
+---@enum Enums.RDMPidValueDisplayInvert
+---@enum Enums.RDMPidValueFactoryDefaults
+---@enum Enums.RDMPidValueOnOff
+---@enum Enums.RDMPidValueResetDevice
+---@enum Enums.RDMPowerStateDefines
+---@enum Enums.RDMProductCategory
+---@enum Enums.RDMProductDetail
+---@enum Enums.RDMResponseNackReason
+---@enum Enums.RDMSensorType
+---@enum Enums.RDMSensorUnit
+---@enum Enums.RDMSensorUnitPrefix
+---@enum Enums.RDMSlotId
+---@enum Enums.RDMSlotType
+---@enum Enums.RDMStatusMessageType
+---@enum Enums.RealtimeCmdSource
+---@enum Enums.RealtimeCmdType
+---@enum Enums.RealtimeSection
+---@enum Enums.RecordGo
+---@enum Enums.ReductionPolicy
+---@enum Enums.RelationType
+---@enum Enums.ReleaseType
+---@enum Enums.RemoteMidiType
+---@enum Enums.RemoteResolution
+---@enum Enums.RequestedSize
+---@enum Enums.ResolutionLimit
+---@enum Enums.RestartOption
+
+---@enum Enums.Roles
+local Roles = {
+    Default = 0,
+    Display = 1,
+    DisplayShort = 2,
+    Edit = 3,
+    ObjectNoOnly = 7
+}
+
+---@enum Enums.RotationMode
+---@enum Enums.RowOrder
+---@enum Enums.SCVirtualKeyCode
+---@enum Enums.SMPTEMode
+---@enum Enums.SacnDataMode
+---@enum Enums.ScrollItemPlacementType
+---@enum Enums.ScrollParamEntity
+---@enum Enums.ScrollParamValueType
+---@enum Enums.ScrollReason
+---@enum Enums.ScrollType
+---@enum Enums.SectionCount
+---@enum Enums.SelectedEnvironment
+---@enum Enums.SelectedSelection
+---@enum Enums.SelectionMode
+---@enum Enums.SeqMasterGoMode
+---@enum Enums.SeqRestartMode
+---@enum Enums.SequenceAction
+---@enum Enums.SequenceCountdown
+---@enum Enums.SequenceLinkType
+---@enum Enums.SetupType
+---@enum Enums.ShaderTypes
+---@enum Enums.ShadowMode
+---@enum Enums.SheetMode
+---@enum Enums.ShowFileSegmentsMask
+
+---@enum Enums.ShowFileStatus
+local ShowFileStatus = {
+    Undefined = 0,
+    NoShow = 1,
+    Showloaded = 2,
+    ShowDownloaded = 3,
+    DataNegotiationActive = 4,
+    ShowSaving = 5,
+    DataNegotiationMaster = 6,
+    DataNegotiationSlave = 7, 
+    ShowUploading = 8,
+    ShowMediaUploading = 9,
+    ShowDownloading = 10,
+    ShowMediaDownloading = 11,
+    ShowDownloadIgnore = 12
+}
+
+---@enum Enums.ShowPathType
+---@enum Enums.ShowUserEncoder
+---@enum Enums.ShuffleMode
+---@enum Enums.SignalOff
+---@enum Enums.SignalSlot
+---@enum Enums.SoundValues
+---@enum Enums.SpecialAttribute
+---@enum Enums.SpecialCycle
+---@enum Enums.SpecialExecutor
+---@enum Enums.SpecialValueTypes
+---@enum Enums.SpecialValues
+---@enum Enums.SpeedMaster
+---@enum Enums.SpeedReadoutMode
+---@enum Enums.SpeedReadoutModeAuto
+---@enum Enums.SpeedScale
+---@enum Enums.SplineType
+---@enum Enums.SplitterH_LeftRight
+---@enum Enums.SplitterV_TopBottom
+---@enum Enums.StoreMode
+---@enum Enums.StoreSource
+---@enum Enums.StoremodeRadio
+---@enum Enums.SyntaxResult
+---@enum Enums.SyntaxState
+---@enum Enums.TCDuration
+---@enum Enums.TCTextMode
+---@enum Enums.TCViewMode
+---@enum Enums.TestBlockType
+---@enum Enums.TimeCodeSource
+---@enum Enums.TimeCursorScrollMode
+---@enum Enums.TimeDefault
+---@enum Enums.TimeDisplayClockFormat
+---@enum Enums.TimeDisplayFormat
+---@enum Enums.TimeDisplayFormatSelected
+---@enum Enums.TimeKeyTarget
+---@enum Enums.TimeNone
+---@enum Enums.TimecodeEncoderFunction
+---@enum Enums.TimecodeSelectLastEventMode
+---@enum Enums.TimecodeSelectionTarget
+---@enum Enums.TimecodeSlot
+---@enum Enums.TimecodeSlotOff
+---@enum Enums.TimecodeSlotShort
+---@enum Enums.TimecodeTool
+---@enum Enums.TimerAction
+---@enum Enums.TimerLinkType
+---@enum Enums.TimerMode
+---@enum Enums.ToolbarScrollType
+---@enum Enums.TotalReferenceUpdateStatus
+---@enum Enums.TotalUpdateType
+---@enum Enums.TrackEditApplyFor
+---@enum Enums.TrackEditGrouping
+---@enum Enums.TrackLayerAuto
+---@enum Enums.TrackpadMode
+---@enum Enums.TrackpadPTInvertMode
+---@enum Enums.TrackpadPTMode
+---@enum Enums.TransitionMode
+---@enum Enums.TransitionType
+---@enum Enums.TrueFalse
+---@enum Enums.UndefinedAnchors
+---@enum Enums.UndefinedMax
+---@enum Enums.UndefinedMin
+---@enum Enums.Update
+---@enum Enums.UsbProductID
+---@enum Enums.UseLeftOrRight
+
+---@enum Enums.UserRights
+local UserRights = {
+    Admin = 0,
+    Setup = 1,
+    Program = 2,
+    Presets = 3,
+    Playback = 4,
+    View = 5,
+    None = 6
+}
+
+---@enum Enums.ValueLayer
+---@enum Enums.ValueNone
+---@enum Enums.ValueReadoutMode
+---@enum Enums.ValueReadoutModeAuto
+---@enum Enums.ValueReadoutModeDefault
+---@enum Enums.ValueReadoutModeNatural
+---@enum Enums.ValueRole
+---@enum Enums.VerifyResult
+
+---@enum Enums.VideoFileSource
+local VideoFileSource = {
+    File = 0,
+    NDI = 1
+}
+
+---@enum Enums.VirtualKeyCode
+---@enum Enums.VirtualKeyExecutionType
+---@enum Enums.WeekDay
+---@enum Enums.WeekDayShort
+---@enum Enums.WeekOfMonth
+---@enum Enums.WheelMode
+---@enum Enums.WhiteListPacketNames
+---@enum Enums.WhiteListPacketNamesBefore_1_7
+---@enum Enums.WindowCategories
+---@enum Enums.WindowTypes
+---@enum Enums.WingID
+---@enum Enums.WingType
+---@enum Enums.XFadeMode
+---@enum Enums.XYZMapping
+---@enum Enums.Yes
+---@enum Enums.YesNo
+---@enum Enums.ZoomFactor
