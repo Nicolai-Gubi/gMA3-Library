3f6ccba577740f11115d8995f631b99f6fb498b8
Nicolai-Gubi <113240111+Nicolai-Gubi@users.noreply.github.com>
2023-08-05 09:56:37 +0200

    Create GetUIChannels_handles.lua

---
+++ manual_examples/GetUIChannels_handles.lua
@@ -0,0 +1,12 @@
+return function()
+    local fixtureHandle = GetSubfixture(SelectionFirst())
+    -- Creates a table of handles to the UI channels of the first selected fixture.
+    local uiChannels = GetUIChannels(fixtureHandle, true)
+    if uiChannels == nil then
+        ErrPrintf("Please select a fixture and try again")
+        return
+    end
+    for key,value in pairs(uiChannels) do
+        Printf(key .. ": UIChannel Index = %i, (Sub)Attribute = %s", value.INDEX-1, value.SUBATTRIBUTE)
+	end
+end
